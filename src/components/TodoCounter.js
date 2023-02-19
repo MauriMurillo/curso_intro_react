8a1d54cebc0fdf27315783af44610c609177df00
Mauricio Murillo Gonzales <mauriciomurillogonzales@Mauricios-MacBook-Pro.local>
2023-02-18 23:35:24 -0700

    Updates

--- src/components/TodoCounter.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import './styles_component/TodoCounter.css'
-function TodoCounter({total, done}){
-  return(
-    <h1 className="mainHeading"> Has completado {done} de {total} ToDos </h1>
-  );
-}
-export { TodoCounter };
